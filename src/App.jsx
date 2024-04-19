1909226037375a7fbb6b60e8df8e3eb29abdb8ad
Joha Wilson <johawilsonatu@icloud.com>
2024-04-19 19:11:22 +0100

    first commit with basic layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SideBar from "./components/sidebar.jsx";
+
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">React Project Manager App</h1>
+      <SideBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar() {
+    return (
+        <aside>
+            <h2> Your Projects</h2>
+
+            <ul>
+                
+            </ul>
+        </aside>
+    );
+}
