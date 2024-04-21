9bf8eb183373ce74ba1074ed1303f912a53bdd3a
Joha-will <johawilsonatu@icloud.com>
2024-04-21 23:57:53 +0100

    Added some styles with tailwindcss and created Input and New project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import NewProject from "./components/NewProject.jsx";
 import SideBar from "./components/sidebar.jsx";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8">
       <h1 className="my-8 text-center text-5xl font-bold">React Project Manager App</h1>
       <SideBar />
-    </>
+      <NewProject />
+    </main>
   );
 }
 
