3eb0a553cd23d0a0f3a3ba27a40333048f1fcecf
Joha-will <johawilsonatu@icloud.com>
2024-04-23 23:19:51 +0100

    styled buttons and input fields with tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import SideBar from "./components/sidebar.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8">
-      <h1 className="my-8 text-center text-5xl font-bold">React Project Manager App</h1>
+    <main className="h-screen my-8 flex gap-8 ">
       <SideBar />
       <NewProject />
     </main>
