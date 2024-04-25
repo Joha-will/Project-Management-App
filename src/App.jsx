1ae7e7cf3b2413f429c99cbf3c8ff727552e75e5
Joha-will <johawilsonatu@icloud.com>
2024-04-25 22:46:06 +0100

    create Unselected component and split Button into component for better reusability

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject.jsx";
+import UnselectedProject from "./components/UnselectedProject.jsx";
 import SideBar from "./components/sidebar.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8 ">
       <SideBar />
-      <NewProject />
+      <UnselectedProject />
     </main>
   );
 }
